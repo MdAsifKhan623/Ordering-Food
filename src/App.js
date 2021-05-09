ecab83900fca454136790275263658ae13c8bfe9
Asif623 <mdasifk22@gmail.com>
2021-05-09 16:10:15 -0400

    added initial code with server side working

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
+import React,{useEffect,useState} from 'react'
+import axios from 'axios'
 import './App.css';
 
 function App() {
+  let [result,setResult]=useState('')
+  useEffect(()=>{
+    axios.get("http://localhost:5000/").then(res=>{
+      setResult(res.data)
+    })
+  })
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{result}</h1>
     </div>
   );
 }
