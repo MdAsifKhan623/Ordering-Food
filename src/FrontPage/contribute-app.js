960874a29d082fd66b23fb187c9ef03d79fc0c43
Asif623 <mdasifk22@gmail.com>
2021-05-30 23:37:10 -0400

    added some desc

--- src/FrontPage/contribute-app.js
+++ src/FrontPage/contribute-app.js
@@ -6,9 +6,9 @@ export default function Contribute(){
     return (
         <div>
             <div className="food-options">
-                <div className="food-items"><CardComp title="KFC" description={description.kfc} img={kfc}/></div>
-                <div className="food-items"><CardComp title="Walgreens" description={description.walgreens} img={walgreens}/></div>
-                <div className="food-items"><CardComp title="Burger King" description={description.burgerking} img={burger}/></div>                
+                <div className="food-items"><CardComp title="Download App" description={description.kfc} img={kfc}/></div>
+                <div className="food-items"><CardComp title="Contribute" description={description.walgreens} img={walgreens}/></div>
+                <div className="food-items"><CardComp title="Get Coupons" description={description.burgerking} img={burger}/></div>                
             </div>
         </div>
     )
