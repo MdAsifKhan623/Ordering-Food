8a75398c3bc2d30a361cf2d5be5793fecc1ee777
Asif623 <mdasifk22@gmail.com>
2021-07-07 23:28:09 -0400

    s

--- src/SignIn/signup.js
+++ src/SignIn/signup.js
@@ -24,6 +24,7 @@ export default function SignUp(){
 
     return (
         <div  className="signup-box">
+            <h1><center>Sign Up</center></h1>
             <div className="signup">
                 <form onSubmit={registerHandler}>
                     <fieldset>
