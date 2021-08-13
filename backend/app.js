31f48e092c37466151573f7bb6423aa15e4fed90
Asif623 <mdasifk22@gmail.com>
2021-08-12 23:55:33 -0400

    added some db changes

--- backend/app.js
+++ backend/app.js
@@ -1,13 +1,25 @@
-const dbConnection =require('./database')
-
-dbConnection()
-
+const mongoose = require('mongoose')
 const express= require('express')
 const cors=require('cors')
+const { Connection } = require('mongoose')
 const app=express()
 app.use(cors())
 
+const databaseName="food-order"
+
+mongoose.connect(`mongodb://127.0.0.1:21017/${databaseName}`,{
+            useNewUrlParser:true,
+            useUnifiedTopology:true,
+            useCreateIndex:true});
+
+const connection =mongoose.connection;
+
+connection.once("open",()=>{
+    console.log("Connection to Mongo Db Was successful!")
+})
+
 app.options("*",cors())
+
 
 app.get("/",(req,res)=>{
     res.send("Hungry Bunny")
